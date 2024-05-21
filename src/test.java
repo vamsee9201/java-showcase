66ccbc8155a3dc7928a38a70c2c2178572c1b870
vamsee9201 <vamseekrishna9201@gmail.com>
2024-05-20 21:11:04 -0500

    "commited"

--- src/test.java
+++ src/test.java
@@ -4,7 +4,7 @@ public class test {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("test class");
+        System.out.println("test class test again");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
