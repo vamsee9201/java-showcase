0713d2b93360f46048f4555e97b6cb3e0d7c9a83
vamsee9201 <vamseekrishna9201@gmail.com>
2024-05-25 22:18:04 -0500

    practicing list in collection

--- src/collection1.java
+++ src/collection1.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class collection1 {
+    // this is about List collection interface
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("banana");
+        list.add("banana"); //duplicates are allowed
+        for (String fruit:list) {
+            System.out.println(fruit);
+        }
+
+        list.remove("banana");
+        System.out.println(list);
+        System.out.println("List contains Banana: " + list.contains("banana"));
+        System.out.println("Size of the list: " + list.size());
+        System.out.println("List just cleared");
+        list.clear();
+        System.out.println("Size of the list: " + list.size());
+    }
+
 }
