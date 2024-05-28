fa08dbf5cd43749ab39f7311fd54607d3300d454
vamsee9201 <vamseekrishna9201@gmail.com>
2024-05-27 21:05:38 -0500

    java practice

---
+++ src/AbstractPractice1.java
@@ -0,0 +1,2 @@
+public class AbstractPractice1 {
+}
--- src/compileTimePolymorphism.java
+++ src/compileTimePolymorphism.java
@@ -1,6 +1,22 @@
 
 public class compileTimePolymorphism {
     public static void main(String[] args) {
+    MathOperations math = new MathOperations();
+    System.out.println(math.add(5,7));
+        System.out.println(math.add(7,8,9));
+        System.out.println(math.add(5.5,7.5));
 
+    }
+}
+
+class MathOperations {
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int add(int a, int b, int c){
+        return a+b+c;
+    }
+    public double add(double a,double b){
+        return a+b;
     }
 }
