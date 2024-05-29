95d0970c9d0962dd1445173e21fee6954a0914db
vamsee9201 <vamseekrishna9201@gmail.com>
2024-05-28 22:09:18 -0500

    practiced interface

--- src/AbstractPractice1.java
+++ src/AbstractPractice1.java
@@ -1,2 +1,29 @@
+
+interface Flyable {
+    void Fly();
+}
+interface Swimmable {
+    void swim();
+}
+class  Duck implements Flyable,Swimmable {
+
+    @Override
+    public void Fly() {
+        System.out.println("duck is flying");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("duck is swimming");
+    }
+}
+
+
+
 public class AbstractPractice1 {
+    public static void main(String [] args) {
+        Duck duck = new Duck();
+        duck.Fly();
+        duck.swim();
+    }
 }
