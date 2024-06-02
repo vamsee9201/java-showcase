4018414434c0fed3e8029a28c6cb0164b29ba039
vamsee9201 <vamseekrishna9201@gmail.com>
2024-06-01 21:57:29 -0500

    practicing abstarct

--- src/AbstractPractice2.java
+++ src/AbstractPractice2.java
@@ -1,3 +1,23 @@
+
+abstract class Shape {
+    abstract double calculateArea();
+
+    abstract double calculatePerimeter();
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    double calculateArea(double radius) {
+        return 3.14*radius*radius;
+    }
+
+    double calculatePerimeter(double radius) {
+        return 3.14*2*radius;
+    }
+}
+
+
 public class AbstractPractice2 {
 public static void main(String[] args) {
 
